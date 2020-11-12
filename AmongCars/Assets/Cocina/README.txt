16187dec8379cc9d297ee0f37c4c3645e563dc1c
Marcial <nomaspecesfritos@hotmail.com>
2020-11-12 23:37:01 +0100

    Assets añadidos

---
+++ AmongCars/Assets/Cocina/README.txt
@@ -0,0 +1,7 @@
+Kitchen Props and Food Asset:
+	Low poly
+	For Console & PC & VR
+	23 Prefabs
+	2048x2048 Textures
+	Demo scene
+Enjoy!
---
+++ AmongCars/Assets/Otro Elevador/Script/Moveliftcar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveliftcar : MonoBehaviour {
+
+
+
+		public Transform target;
+		public float speed = 0.0f;
+		public KeyCode Key1;
+		public KeyCode Key2;
+		public Vector3 forwardPos;
+		public Vector3 rearPos;
+
+
+
+		void Update () {
+
+
+			if (Input.GetKey(Key1))
+
+			{
+
+
+
+				target.transform.localPosition = Vector3.MoveTowards(target.transform.localPosition, forwardPos, speed  * Time.deltaTime);
+
+			}
+			if (Input.GetKey(Key2))
+			{
+				target.transform.localPosition = Vector3.MoveTowards (target.transform.localPosition, rearPos, speed * Time.deltaTime);
+			}
+		}
+	}
