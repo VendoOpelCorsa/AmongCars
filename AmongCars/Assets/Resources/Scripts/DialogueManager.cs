8b8df09ed084d19fdf360dfe9255cf6e88ca2683
Sofía Martín <sofimrtn@gmail.com>
2020-11-21 21:12:51 +0100

    Prueba diálogo y colocación de personajes
    El único personaje con el que se puede interactuar de momento es Turquesa.

---
+++ AmongCars/Assets/Resources/Scripts/DialogueManager.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Runtime.CompilerServices;
+
+public class DialogueManager : MonoBehaviour
+{
+    public NPC npc;
+    public TMP_Text npcName;
+    public Image npcImage;
+
+    public GameObject player;
+    
+    Queue<string> sentences;
+
+    public GameObject dialoguePanel;
+    public TMP_Text displayText;
+
+    public GameObject optionsPanel;
+
+    public int curResponseTracker = 0;
+
+    public Button button1;
+    public Button button2;
+    public Button button3;
+
+    public Button btOut;
+
+    public Text op1;
+    public Text op2;
+    public Text op3;
+
+    string activeSentence;
+    public float typingSpeed;
+    
+    AudioSource myAudio;
+    public AudioClip speakSound;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {   
+            print("Prueba");
+            player = other.gameObject;
+            dialoguePanel.SetActive(true);
+            npcName.text = npc.name;
+            string url = "Sprites/" + npc.icon;
+            npcImage.sprite = Resources.Load<Sprite>(url);
+            StartDialogue();
+
+        }
+    }
+
+    void StartDialogue()
+    {
+        sentences.Clear();
+        foreach (string sentence in npc.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public  void DisplayNextSentence()
+    {
+        if (sentences.Count <= 0)
+        {
+            displayText.text = activeSentence;
+            return;
+        }
+
+        activeSentence = sentences.Dequeue();
+        displayText.text = activeSentence;
+        
+        StopAllCoroutines();
+        StartCoroutine(TypeTheSentence(activeSentence));
+    }
+
+    IEnumerator TypeTheSentence(string sentence)
+    {
+        displayText.text = "";
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            displayText.text += letter;
+            //myAudio.PlayOneShot(speakSound);
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        ShowOptions(); 
+    }
+
+    
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {     
+            if(Input.GetKeyDown(KeyCode.I)){
+                ChooseOption(1);
+            }
+            if(Input.GetKeyDown(KeyCode.O)){
+                ChooseOption(2);
+                //button2.AddListener(()=>ChooseOption(2));
+            }
+            if(Input.GetKeyDown(KeyCode.P)){
+                ChooseOption(3);
+                //button3.AddListener(()=>ChooseOption(3));
+            }
+            //button1.onClick.AddListener(() => ChooseOption(1));
+            //button2.onClick.AddListener(() => ChooseOption(2));
+            //button3.onClick.AddListener(() => ChooseOption(3));
+            
+            if (optionsPanel.activeInHierarchy == false )
+            {
+                btOut.onClick.AddListener(() => ClosePanel());
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    ClosePanel();
+                    StopAllCoroutines();
+                }
+            }
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ClosePanel();
+            StopAllCoroutines();
+        }
+    }
+
+    public void ShowOptions() 
+    {
+        optionsPanel.SetActive(true);
+        string[] options = npc.GetOptions();
+        op1.text = options[0];
+        op2.text = options[1];
+        op3.text = options[2];
+    }
+
+    private void ChooseOption(int button)
+    {
+        switch (button)
+        {
+            case 1: 
+                NextSentence(1);
+                break;
+            case 2:
+                NextSentence(2);
+                break;
+            case 3:
+                 NextSentence(3);
+                break;
+        }
+    }
+
+    private void NextSentence(int curResponseTracker) {
+
+        displayText.text = npc.GetSentence(curResponseTracker);
+        optionsPanel.SetActive(false);
+    }
+
+    private void ClosePanel() 
+    {
+        dialoguePanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+}
+
+
+
+
+
---
+++ AmongCars/Assets/Resources/Scripts/NPC.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "NPC file", menuName = "NPC files Archive")]
+public class NPC : ScriptableObject
+{
+    public string name;
+    public string icon;
+
+    [TextArea(3, 15)]
+    public string[] sentences;
+    
+     [TextArea(3, 15)]
+    public string[] playerResponses;
+
+    public string[] GetSentences()
+    {   
+        return sentences;
+    }
+
+    public string GetSentence(int index)
+    {
+        return sentences[index];
+    }
+
+    public string[] GetOptions()
+    {
+        return playerResponses;
+    }
+
+    public string GetResponse(int index)
+    {
+        return playerResponses[index];
+    }
+
+}
