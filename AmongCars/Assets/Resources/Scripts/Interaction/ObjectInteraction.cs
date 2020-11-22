eafecf64df1e2a57483b8f1627490ba62fd12849
UO258935 <UO258935@uniovi.es>
2020-11-22 13:29:08 +0100

    obj inter

--- AmongCars/Assets/Resources/Scripts/Interaction/ObjectInteraction.cs
+++ AmongCars/Assets/Resources/Scripts/Interaction/ObjectInteraction.cs
@@ -5,5 +5,18 @@ public class ObjectInteraction : Interactive
 {
     public String text;
 
-    public override void OnClick() => print(text);
+    public override void OnClick(){
+        print(text);
+
+    /*
+        player = other.gameObject;
+        dialoguePanel.SetActive(true);
+        npcName.text = npc.name;
+        string url = "Sprites/" + npc.icon;
+        npcImage.sprite = Resources.Load<Sprite>(url);
+        StartDialogue();   
+        */ 
+    } 
+
+
 }
