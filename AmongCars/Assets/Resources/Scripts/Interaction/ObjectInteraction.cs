d7734cc310a629ef852b478b719102e5b6405cf7
Marcos <marcosav2010@gmail.com>
2020-11-22 12:27:33 +0100

    Base para interacturar con objetos

---
+++ AmongCars/Assets/Resources/Scripts/Interaction/ObjectInteraction.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+public class ObjectInteraction : Interactive
+{
+    public String text;
+
+    public override void OnClick() => print(text);
+}
--- AmongCars/Assets/Resources/Scripts/Interaction/Objects/PlantInteraction.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class PlantInteraction : OnceInteractive
-{
-    public override void OnClick() => print("awdawd");
-}
