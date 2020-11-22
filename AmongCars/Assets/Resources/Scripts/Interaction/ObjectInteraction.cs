44ce9c02870a89270205d488f8996f11adc5db84
Marcos <marcosav2010@gmail.com>
2020-11-22 14:08:25 +0100

    Interaccion con objetos mejorada

--- AmongCars/Assets/Resources/Scripts/Interaction/ObjectInteraction.cs
+++ AmongCars/Assets/Resources/Scripts/Interaction/ObjectInteraction.cs
@@ -1,22 +1,51 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 public class ObjectInteraction : Interactive
 {
-    public String text;
 
-    public override void OnClick(){
+    private AmongCars controls;
+
+    public String text;
+    public String title;
+
+    private TMP_Text titleTX;
+    private TMP_Text displayTX;
+    private GameObject dialoguePanel;
+
+    private bool open;
+
+    protected void Awake() => controls = new AmongCars();
+    protected void OnEnable() => controls?.Enable();
+    protected void OnDisable() => controls?.Disable();
+
+    public override void OnClick()
+    {
+        open = true;
         print(text);
 
-    /*
-        player = other.gameObject;
         dialoguePanel.SetActive(true);
-        npcName.text = npc.name;
-        string url = "Sprites/" + npc.icon;
-        npcImage.sprite = Resources.Load<Sprite>(url);
-        StartDialogue();   
-        */ 
-    } 
+        titleTX.text = title;
 
+        displayTX.text = text;
+    }
 
+    void Start()
+    {
+        dialoguePanel = GameObject.FindWithTag("dialoguePanel");
+
+        titleTX = GameObject.FindWithTag("title").GetComponent<TMP_Text>();
+        displayTX = GameObject.FindWithTag("displayText").GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        if (open)
+            if (controls.Player.Salir.ReadValue<float>() == 1)
+            {
+                dialoguePanel.SetActive(false);
+                open = false;
+            }
+    }
 }
