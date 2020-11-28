ba715493de1494446cc45b841803ad4bc82a3290
Sara Garcia Rodriguez <47144645+SaraGarcia13@users.noreply.github.com>
2020-11-28 14:59:15 +0100

    Aviso de qué tarea tienes que hacer

--- AmongCars/Assets/Resources/Scripts/Work.cs
+++ AmongCars/Assets/Resources/Scripts/Work.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Runtime.CompilerServices;
 
 public class Work : MonoBehaviour
 {
     public AmongCars controls;
     public GameObject panel;
+    public TMP_Text taskText;
+
     public float time = 1;
+
+    private List<string> tasks;
 
     protected void Awake() => controls = new AmongCars();
     protected void OnEnable() => controls?.Enable();
@@ -14,6 +21,10 @@ public class Work : MonoBehaviour
 
     void Start()
     {
+        tasks = new List<string>();
+        tasks.Add("Vete fuera donde los contenedores a sincronizar los generadores");
+        tasks.Add("Vaya... tienes suerte. No hay nada que hacer... de momento");
+        tasks.Add("Es bromi");
         panel.SetActive(false);
         StartCoroutine(TimeToWork());
     }
@@ -22,8 +33,15 @@ public class Work : MonoBehaviour
     {
         yield return new WaitForSeconds(time); 
         panel.SetActive(true);
+        GenerateTask();
         yield return new WaitForSeconds(5); 
         reset();
+    }
+
+    void GenerateTask()
+    {
+        int index = Random.Range(0, tasks.Count);
+        taskText.text = tasks[index];
     }
 
     void reset()
