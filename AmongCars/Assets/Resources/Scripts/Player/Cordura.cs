0a75f99e755bcb472281ee4700f4d91901b9c653
Sofía Martín <sofimrtn@gmail.com>
2020-11-29 00:48:14 +0100

    metodo recuperar cordura para laberinto

--- AmongCars/Assets/Resources/Scripts/Player/Cordura.cs
+++ AmongCars/Assets/Resources/Scripts/Player/Cordura.cs
@@ -9,22 +9,19 @@ public class Cordura : MonoBehaviour
 
     public Image textoCordura;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         cordura = corduraTotal;
     }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
-
     void quitarCordura(){
         cordura = Mathf.Clamp(cordura - (corduraTotal/3), 0f, corduraTotal);
         print(cordura);
+        textoCordura.transform.localScale = new Vector2( cordura/corduraTotal, 1);
+    }
+
+    void recuperarCordura(){
+        cordura = corduraTotal;
         textoCordura.transform.localScale = new Vector2( cordura/corduraTotal, 1);
     }
 
