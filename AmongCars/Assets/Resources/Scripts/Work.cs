40c4baac5bc64e40347aabd3cdcb4b35d8281107
Sara Garcia Rodriguez <47144645+SaraGarcia13@users.noreply.github.com>
2020-11-28 14:20:56 +0100

    Aviso trabajo
    Cada X tiempor aparece un aviso de Naranja mandándote trabajar

---
+++ AmongCars/Assets/Resources/Scripts/Work.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Work : MonoBehaviour
+{
+    public AmongCars controls;
+    public GameObject panel;
+    public float time = 1;
+
+    protected void Awake() => controls = new AmongCars();
+    protected void OnEnable() => controls?.Enable();
+    protected void OnDisable() => controls?.Disable();
+
+    void Start()
+    {
+        panel.SetActive(false);
+        StartCoroutine(TimeToWork());
+    }
+
+    IEnumerator TimeToWork()
+    {
+        yield return new WaitForSeconds(time); 
+        panel.SetActive(true);
+        yield return new WaitForSeconds(5); 
+        reset();
+    }
+
+    void reset()
+    {
+        panel.SetActive(false);
+        StartCoroutine(TimeToWork());
+    }
+}
