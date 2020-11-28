7a77413b74466eabe41422514f9e0dbfb150bdaa
UO258935 <UO258935@uniovi.es>
2020-11-28 19:09:29 +0100

    door interaction

---
+++ AmongCars/Assets/Resources/Scripts/Interaction/DoorInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteraction : ObjectInteraction
+{
+    public KeyInteraction key;
+   public override void OnInteract()
+    {
+        base.OnInteract();
+
+        if(key.Found){
+            gameObject.SetActive(false);
+        }
+        
+    }
+}
