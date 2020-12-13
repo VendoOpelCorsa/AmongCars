1ef11c108b7d3842e5ac8012a3cb1f50f3158e33
Sara Garcia Rodriguez <47144645+SaraGarcia13@users.noreply.github.com>
2020-12-13 20:03:22 +0100

    fin del juego cuando te vuelves crazy

--- AmongCars/Assets/Resources/Scripts/Player/Cordura.cs
+++ AmongCars/Assets/Resources/Scripts/Player/Cordura.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Cordura : MonoBehaviour
 {   
     public float cordura, corduraTotal = 100f;
 
     public Image textoCordura;
+
+    public GameObject panelFinal;
+    public Text panelSimon;
+    public Text panelAcusar;
 
     void Start()
     {
@@ -18,6 +23,21 @@ public class Cordura : MonoBehaviour
         cordura = Mathf.Clamp(cordura - (corduraTotal/3), 0f, corduraTotal);
         print(cordura);
         textoCordura.transform.localScale = new Vector2( cordura/corduraTotal, 1);
+
+        if(cordura == 0)
+        {
+           panelSimon.GetComponent<Text>().text = "";
+           panelAcusar.GetComponent<Text>().text = "";
+           panelFinal.SetActive(true);
+           FinJuego();
+           SceneManager.LoadScene("MainMenu"); 
+        }
+    }
+
+    IEnumerator FinJuego()
+    {
+        print("entro");
+        yield return new WaitForSeconds(20); 
     }
 
     void recuperarCordura(){
